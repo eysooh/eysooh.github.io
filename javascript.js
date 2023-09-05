2ef739927fb75978a13da29229dca82d5fd3b442
eysooh <eysooh@gmail.com>
2023-09-05 13:46:58 -0500

    v.2.1

--- javascript.js
+++ javascript.js
@@ -1,29 +1,9 @@
-const themeToggleInput = document.getElementById("theme-toggle-input");
 const themeLink = document.getElementById("theme");
 const logoImgEysooh = document.getElementById("logo-imgEysooh");
 const logoImg = document.getElementById("logo-img");
 const navIcon = document.querySelector('.nav-icon');
 const navLinks = document.querySelector('.nav-links');
 const logo = document.querySelector('.logo');
-
-
-
-
-let currentTheme = "styledark.css";
-let logoEysoohToggle = false;
-let logoToggle = false;
-
-themeToggleInput.addEventListener("change", () => {
-    currentTheme = currentTheme === "styledark.css" ? "stylewhite.css" : "styledark.css";
-    themeLink.setAttribute("href", currentTheme);
-});
-
-themeToggleInput.addEventListener("click", () => {
-    logoImgEysooh.setAttribute("src", logoEysoohToggle ? "./img/logoEysooh.png" : "./img/logoEysooh2.png");
-    logoEysoohToggle = !logoEysoohToggle;
-    logoImg.setAttribute("src", logoToggle ? "./img/logo.png" : "./img/logo2.png");
-    logoToggle = !logoToggle;
-});
 
 navIcon.addEventListener('click', () => {
     navLinks.classList.toggle('active');
